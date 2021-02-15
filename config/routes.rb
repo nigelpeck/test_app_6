fbcd72c2b9119f61d3989abb7298de7a9ae368ed
Nigel Peck <nigelpeck1966@gmail.com>
2021-02-15 13:12:31 +0000

    scaffold generation for Article

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # root 'application#hello' # actually means application_controller - it's implicit - then the # sign to denote the method that you want to run
 
